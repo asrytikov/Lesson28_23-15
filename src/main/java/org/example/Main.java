27f1fc22e8dc6668834838888a1bf8fc1e5cb9fc
asrytikov <asrytikov@gmail.com>
2023-11-15 11:49:27 +0400

    Lesson28

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
 
-        var context = new AnnotationConfigApplicationContext();
-        Person person = new Person();
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Person person = context.getBean(Person.class);
+        System.out.println(person.getName());
+
+        String s = context.getBean(String.class);
+        System.out.println(s);
+
+        Integer i = context.getBean(Integer.class);
+        System.out.println(i);
     }
 }
--- src/main/java/org/example/ProjectConfig.java
+++ src/main/java/org/example/ProjectConfig.java
@@ -1,7 +1,41 @@
 package org.example;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ProjectConfig {
+
+    @Primary
+    @Bean(name="alex")
+    Person person(){
+        var p = new Person();
+        p.setName("Alex");
+        return p;
+    }
+
+    @Bean(value = "masha")
+    Person person2(){
+        var p = new Person();
+        p.setName("Masha");
+        return p;
+    }
+
+    @Bean("igor")
+    Person person3(){
+        var p = new Person();
+        p.setName("Igor");
+        return p;
+    }
+
+    @Bean
+    String hello(){
+        return "Hello";
+    }
+
+    @Bean
+    Integer count(){
+        return 10;
+    }
 }
